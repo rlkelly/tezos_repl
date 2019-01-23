9b0305de419ddb9a6ff6a4b4347c682ae737ee79
Robert L Kelly <robert.l.kelly3@gmail.com>
2019-01-23 02:04:54 -0500

    removed wildcards

--- objects/__init__.py
+++ objects/__init__.py
@@ -1,3 +1,8 @@
-from .basic_types import *
-from .tezos_types import *
+from .basic_types import (
+    Unit, Bytes, Operation, NoneType, Nat, Int, Timestamp, Address,
+    Mutez, Bool, String
+)
+from .tezos_types import (
+    Lambda, List, Set, Map, BigMap, Or, Some, Pair
+)
 from .utils import deep_compare
